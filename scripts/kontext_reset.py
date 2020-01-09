849c463b1c22e29b89c3c18947ed961108c4e0e8
Tomas Machalek <tomas.machalek@gmail.com>
2020-01-09 11:09:40 +0100

    Update corpus refresh script to reflect latest changes

--- scripts/kontext_reset.py
+++ scripts/kontext_reset.py
@@ -1,6 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import celery
 import requests
+
+"""
+This script can be used to refresh information about a corpus
+in case the corpus data changed (typically a 'monitor' corpus).
+
+Please do not forget to check/update the configuration constants
+below to match your actual setup.
+
+Usage:
+
+a) as an imported module:
+
+import kontext_reset
+refresh_corpus_props('corpus_name')
+
+b) via command line:
+
+./kontext_reset.py corpus_name
+"""
 
 celery_conf = dict(
 	BROKER_URL = 'redis://192.168.1.34:6379/12',
@@ -10,8 +29,9 @@ celery_conf = dict(
 	CELERY_ACCEPT_CONTENT = ['json'])
 
 MASM_API_RESTART_URL = 'http://kontext5:8080/kontext-services/soft-reset-all'
+MASM_API_CORPUS_DB_UPDATE_URL = 'http://kontext5:8080/corpora-database/{}/auto-update'
 
-def clean_cache(corpname):
+def _clean_cache(corpname):
     worker = celery.Celery('bgcalc', config_source=celery_conf)
     # args=(ttl, subdir, dry_run, corpus_id)
     ans = worker.send_task('conc_cache.conc_cache_cleanup', args=(0, '', False, corpname))
@@ -19,14 +39,39 @@ def clean_cache(corpname):
     # response: {'deleted': 4, 'type': 'summary', 'processed': 4}
     return task_result
 
-def reload_service():
+def _reload_service():
     ans = requests.post(MASM_API_RESTART_URL)
     if ans.status_code == 200:
         return ans.json()
     else:
         ans.raise_for_status()
 
+def _update_corpora_db(corpname):
+    ans = requests.post(MASM_API_CORPUS_DB_UPDATE_URL.format(corpname))
+    if ans.status_code == 200:
+        return ans.json()
+    else:
+        ans.raise_for_status()
+
+def refresh_corpus_props(corpname):
+    """
+    The function performs all the necessary
+    steps to refresh information about corpus.
+    """
+    return dict(
+        clean_cache=_clean_cache(corpname),
+        reload_service=_reload_service(),
+        update_corpora_db=_update_corpora_db(corpname)
+    )
+
+
 if __name__ == '__main__':
     import sys
-    print(clean_cache(sys.argv[1]))
-    print(reload_service())
+    if len(sys.argv) < 2:
+        print('Missing corpus name')
+        sys.exit(1)
+    try:
+        print(refresh_corpus_props(sys.argv[1]))
+    except Exception as e:
+        print('{}: {}'.format(e.__class__.__name__, e))
+        sys.exit(1)
