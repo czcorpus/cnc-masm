fa1c57d645d6e5a74d83d1b5ef96c0b21e76dadf
Tomas Machalek <tomas@machalek.xyz>
2022-06-15 17:39:15 +0200

    add conf viewing action, move conf building to a separate function

--- liveattrs/actions.go
+++ liveattrs/actions.go
@@ -116,6 +116,31 @@ func groupBibItems(data *qans.QueryAns, bibLabel string) {
 	}
 }
 
+func createLAConfig(corpusInfo *corpus.Info, atomStructure string, bibView bool, bibIdAttr string) *cnf.NewVTEConf {
+	newConf := cnf.NewVTEConf{}
+	newConf.Corpus = corpusInfo.ID
+	newConf.VerticalFile = corpusInfo.RegistryConf.Vertical.Path
+	newConf.Encoding = corpusInfo.RegistryConf.Encoding
+	newConf.AtomStructure = atomStructure
+	newConf.StackStructEval = false
+	newConf.Structures = corpusInfo.RegistryConf.SubcorpAttrs
+	if bibView {
+		bibView := vteDb.BibViewConf{}
+		bibView.IDAttr = bibIdAttr
+		for stru, attrs := range corpusInfo.RegistryConf.SubcorpAttrs {
+			for _, attr := range attrs {
+				bibView.Cols = append(bibView.Cols, fmt.Sprintf("%s_%s", stru, attr))
+			}
+		}
+		newConf.BibView = &bibView
+	}
+	// TODO
+	newConf.Ngrams = nil
+	newConf.SelfJoin = nil
+
+	return &newConf
+}
+
 // Actions wraps liveattrs-related actions
 type Actions struct {
 	exitEvent   <-chan os.Signal
@@ -127,6 +152,21 @@ type Actions struct {
 }
 
 func (a *Actions) OnExit() {}
+
+func (a *Actions) ViewConf(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	corpusID := vars["corpusId"]
+	conf, err := a.laConfCache.Get(corpusID)
+	if err != nil {
+		api.WriteJSONErrorResponse(w, api.NewActionError("error fetching configuration: %s", err), http.StatusBadRequest)
+		return
+	}
+	if conf == nil {
+		api.WriteJSONErrorResponse(w, api.NewActionError("Configuration not found"), http.StatusNotFound)
+		return
+	}
+	api.WriteJSONResponse(w, conf)
+}
 
 // Create handles creating of liveattrs data for a specific corpus.
 // The verticals to be processed are by default defined in a respective
@@ -148,28 +188,13 @@ func (a *Actions) Create(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 
-		newConf := cnf.NewVTEConf{}
-		newConf.Corpus = corpusID
-		newConf.VerticalFile = corpusInfo.RegistryConf.Vertical.Path
-		newConf.Encoding = corpusInfo.RegistryConf.Encoding
-		newConf.AtomStructure = req.URL.Query().Get("atomStructure")
-		newConf.StackStructEval = false
-		newConf.Structures = corpusInfo.RegistryConf.SubcorpAttrs
-		if req.URL.Query().Get("bibView") == "1" {
-			bibView := vteDb.BibViewConf{}
-			bibView.IDAttr = req.URL.Query().Get("bibIdAttr")
-			for stru, attrs := range corpusInfo.RegistryConf.SubcorpAttrs {
-				for _, attr := range attrs {
-					bibView.Cols = append(bibView.Cols, fmt.Sprintf("%s_%s", stru, attr))
-				}
-			}
-			newConf.BibView = &bibView
-		}
-		// TODO
-		newConf.Ngrams = nil
-		newConf.SelfJoin = nil
-
-		err = a.laConfCache.Save(&newConf)
+		newConf := createLAConfig(
+			corpusInfo,
+			req.URL.Query().Get("atomStructure"),
+			req.URL.Query().Get("bibView") == "1",
+			req.URL.Query().Get("bibIdAttr"),
+		)
+		err = a.laConfCache.Save(newConf)
 		if err != nil {
 			api.WriteJSONErrorResponse(w, api.NewActionError("Cannot run liveattrs generator: '%s'", err), http.StatusBadRequest)
 			return
