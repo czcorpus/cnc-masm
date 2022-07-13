7cce9050caccce88df002fd034793f1a4a6b8b28
Martin Zimandl <martin.zimandl@gmail.com>
2022-07-13 15:50:52 +0200

    make conf copy before assining verticalFiles

--- liveattrs/actions.go
+++ liveattrs/actions.go
@@ -259,8 +259,9 @@ func (a *Actions) Create(w http.ResponseWriter, req *http.Request) {
 		}
 	}
 
+	runtimeConf := *conf
 	if len(jsonArgs.VerticalFiles) > 0 {
-		conf.VerticalFiles = jsonArgs.VerticalFiles
+		runtimeConf.VerticalFiles = jsonArgs.VerticalFiles
 	}
 	// TODO search collisions only in liveattrs type jobs
 
@@ -288,7 +289,7 @@ func (a *Actions) Create(w http.ResponseWriter, req *http.Request) {
 		CorpusID: corpusID,
 		Start:    jobs.CurrentDatetime(),
 		Args: jobInfoArgs{
-			VteConf: *conf,
+			VteConf: runtimeConf,
 			Append:  append == "1",
 		},
 	}
